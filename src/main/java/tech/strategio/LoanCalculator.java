16417d1cde7bdb955a4b262f39780fac04e43d30
Adam Wojdyla <awojdyla@gmail.com>
2022-12-09 11:55:10 -0800

    implemented get remaining amount function

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
+     * The method takes in a loan amount due and returns the remaining
+     * balance after 3 consecutive monthly payments of 10% of the loan.
      * 
-     * @param ???
-     * @return ???
+     * @param amount represents the original debt owed.
+     * @return the remaining total of the loan.
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+
+        int timeframe = 3;
+        int amt = amount;
+
+        for (int i = 0; i < timeframe ; i++) {
+            amt = amt - (int)(amt * .1);
+        }
+        return amt;
     }
 
     public static void main(String[] args) {
